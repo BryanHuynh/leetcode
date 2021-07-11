cd428047cdceb55a5ab6900c7e46ec9b4bdd09ae
bryanhuynh <bryan.huynh1@ucalgary.ca>
2021-07-11 15:44:54 -0600

    group anagram complete

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -1,7 +1,18 @@
 from typing import List
 
-def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-    return [[]]
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    result = {}
+    
+    for word in strs:
+        s = ''.join(sorted(word))
+        if s not in result.keys():
+            result[s] = [word]
+        else: 
+            result[s].append(word)
+
+    return(result.values())        
+            
+
 
 if __name__ == '__main__':
-    print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+    print(groupAnagrams(["eat","tea","tan","ate","nat","bat",'aab','baa','aba']))
